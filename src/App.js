657cbfb986618e1fe6e9fe5d6c7dea07941ed2eb
tinkugupta5 <36808633+tinkugupta5@users.noreply.github.com>
2023-05-01 13:16:12 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Add from "./Add";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nave from "./Nav";
 import Employe from "./Employe.js";
+// import BoxShadowCard from "./BoxShadowCard";
+// import BoxShadowCards from "./BoxShadowCard";
 
 const App = () => {
   return (
@@ -11,7 +13,8 @@ const App = () => {
         <>
           <Routes>
             <Route path="/" element={<Add />} />
-            <Route path="/view" element={<Employe/>} />
+            <Route path="/view" element={<Employe />} />
+            {/* <Route path="/box" element={<BoxShadowCards />} /> */}
           </Routes>
         </>
       </div>
